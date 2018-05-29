077bcf6f8fc1e69dd40a594a82334e7ec30e4a28
naumenko-sa <evolgenomicslab@gmail.com>
2018-05-29 16:45:03 -0400

    new file:   crg.sv.prioritise.sh

---
+++ crg.sv.prioritise.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#####################
+# parameters:
+#   $1 = case = project = family
+#   $2 = panel.bed - bed file with gene coordinates, created with ~/bioscripts/genes.R
+
+#get only PASS calls
+sample=`bcftools query -l ${1}-metasv.vcf.gz`
+
+bcftools view -f .,PASS -o $sample.pass.vcf.gz -Oz ${1}-metasv.vcf.gz
+tabix $sample.pass.vcf.gz
+
+bedtools intersect -a $sample.pass.vcf.gz -b $2 -header > $sample.pass.region.vcf
+bgzip $sample.pass.region.vcf
+tabix $sample.pass.region.vcf.gz
