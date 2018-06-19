9224010dce1445781a86b6c808793efc52ee9119
naumenko-sa <evolgenomicslab@gmail.com>
2018-06-19 16:10:23 -0400

    new file:   crt.tcag.subset.py

---
+++ crt.tcag.subset.py
@@ -0,0 +1,33 @@
+#!/bin/env python
+
+# subset tcag report using a bed file
+
+import csv
+import sys
+import re
+
+original_report = sys.argv[1]
+
+gene_panel_file_name = sys.argv[2]
+bed = []
+
+with open(gene_panel_file_name,'rb') as f_gene_panel:
+    for line in f_gene_panel:
+	record = line.strip().split('\t')
+	bed.append(record)
+f_gene_panel.close()
+	
+with open(original_report,'rb') as f_original_report:
+    for line in f_original_report:
+	if line.startswith('#'):
+	    header_line = line.strip().replace('\t',',')
+	    print(header_line)
+	else:
+	    record = line.strip().split('\t')
+	    chrom = record[0].replace('chr','')
+	    start = record[1]
+	    for interval in bed:
+		if ((interval[0] == chrom) and (start>=interval[1]) and (start<=interval[2])):
+		    print ','.join(record)
+
+f_original_report.close()
