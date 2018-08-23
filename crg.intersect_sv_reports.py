3f8f6dc2e497fc6ffcd4eecc03ec83c2d3969972
Dennis Kao <d.kao@live.com>
2018-08-23 12:09:55 -0400

    StructuralVariant class now holds all field data instead of random tuples

--- crg.intersect_sv_reports.py
+++ crg.intersect_sv_reports.py
@@ -9,43 +9,74 @@ from pybedtools import BedTool
 
 csv.field_size_limit(sys.maxsize)
 
+class StructuralVariant:
+
+	'''Simple class to hold column values'''
+
+	def __init__(self):
+
+		#Standard BED fields
+		self.chr = ""
+		self.start = ""
+		self.end = ""
+		self.gene = ""
+
+		#Custom implementation fields
+		self.exons_spanned = ""
+
+		#SVSCORES fields
+		self.svlen = ""
+		self.svtype = ""
+		self.svmax = ""
+		self.svsum = ""
+		self.svtop5 = ""
+		self.svtop10 = ""
+		self.svmean = ""
+
+		#TCAG Frequency
+		self.dgv = ""
+
+		#DECIPHER Population values
+		self.del_obs = ""
+		self.del_freq = ""
+		self.del_se = ""
+		self.dup_obs = ""
+		self.dup_freq = ""
+		self.dup_sta = ""
+		self.obs = ""
+		self.freq = ""
+		self.se = ""
+		self.cnv_type = ""
+		self.samp_size = ""
+		self.study = ""
+
+		#DECIPHER CNVS values
+
+	def make_decipher_link(self):
+		return '=hyperlink("https://decipher.sanger.ac.uk/browser#q/{}:{}-{}")'.format(self.chr, self.start, self.end)
+
+	def key(self):
+		return (self.chr, self.start, self.end)
+
+	def interval(self):
+		return '{}:{}-{}:{}'.format(self.chr, self.start, self.end, self.svtype)
+
 class StructuralVariantRecords:
 
 	def __init__(self, _sample_list):
-		self.all_sv = {}
-		self.all_sv_column_data = {}
+		self.grouped_sv = {}
+		self.all_ref_interval_data = {}
 		self.sample_list = _sample_list
 
-	def add_interval(self, ref_interval, new_interval, new_svlen, new_svtype, samp_name):
-		if ref_interval not in self.all_sv.iterkeys():
-			self.all_sv[ref_interval] = {}
+	def add_interval(self, ref_interval, new_interval, column_data, samp_name):
+		if ref_interval not in self.grouped_sv.iterkeys():
+			self.grouped_sv[ref_interval] = {}
+			self.all_ref_interval_data[ref_interval] = column_data[ref_interval]
 
-		if samp_name not in self.all_sv[ref_interval]:
-			self.all_sv[ref_interval][samp_name] = []
+		if samp_name not in self.grouped_sv[ref_interval]:
+			self.grouped_sv[ref_interval][samp_name] = []
 
-		self.all_sv[ref_interval][samp_name].append((new_interval, new_svlen, new_svtype))
-
-	def parse_column_data(self, sample_csv):
-		'''
-			Parse a sample's CSV and store column data associated with an interval to all_sv_column_data.
-		'''
-		with open(sample_csv) as f:
-
-			try: # skip the header
-				next(f)
-			except ValueError: #file is empty
-				return
-
-			for line in csv.reader(f, delimiter=",", quotechar="\""):
-
-				if not line:
-					continue
-
-				chr, start, gt, svtype, svlen, end, sources, nsvt, gene, ann, svmax, svsum, svtop5, svtop10, svmean, dvg = line
-				key = (chr, start, end)
-
-				if key not in self.all_sv_column_data:
-					self.all_sv_column_data[key] = (svlen, svmax, svsum, svtop5, svtop10, svmean, dvg, gene)
+		self.grouped_sv[ref_interval][samp_name].append(column_data[new_interval])
 
 	def make_sample_list_index(self, interval):
 		'''
@@ -80,10 +111,7 @@ class StructuralVariantRecords:
 
 			if s in interval:
 				for variant in interval[s]:
-					chr, start, end = variant[0]
-					svtype = variant[2]
-
-					samp_details.append('{}:{}-{}:{}'.format(chr, start, end, svtype))
+					samp_details.append(variant.interval())
 				all_samp_details.append(';'.join(samp_details))
 			else:
 				all_samp_details.append("NA")
@@ -111,32 +139,31 @@ class StructuralVariantRecords:
 
 		for samp in samples_in_interval:
 			for variant in samples_in_interval[samp]:
-				chr, start, end = variant[0]
-				svlen, svtype = variant[1], variant[2]
 
-				length = abs(int(svlen))
+				length = abs(int(variant.svlen))
 
 				if length > longest:
 					longest = length
-					longest_svtype = svtype
+					longest_svtype = variant.svtype
 
 		return longest_svtype
 
-	def make_spanning_exon_dict(self, exon_bed):
+	def BedTool_make_all_sv(self, bed_name):
+		with open(bed_name, "w") as f:
+			for sv in self.grouped_sv:
+				f.write('{}\t{}\t{}\t{}.\n'.format(sv[0], sv[1], sv[2], bed_name))
+
+		return BedTool(bed_name)
+
+	def calc_exons_spanned(self, exon_bed):
 		'''
-			Returns a dict where each interval maps to the number of exons it spans
+			Populates the field: exons_spanned for all reference intervals
 		'''
 		tmp_bed_name = "tmp_interval.bed"
 		tmp_all_sv_bed_name = "tmp_all_sv.bed"
-		overlapping_exon_count = {}
-
-		# generate temporary bed file containing all intervals
-		with open(tmp_all_sv_bed_name, "w") as f:
-			for sv in self.all_sv:
-				f.write('{}\t{}\t{}\t.\n'.format(sv[0], sv[1], sv[2]))
 
 		exon_ref = BedTool(exon_bed)
-		sample = BedTool(tmp_all_sv_bed_name)
+		sample = self.BedTool_make_all_sv(tmp_all_sv_bed_name)
 
 		for interval in sample:
 
@@ -144,42 +171,58 @@ class StructuralVariantRecords:
 
 			# create a temp bed file with 1 line - the interval of interest
 			with open(tmp_bed_name, "w") as f:
-				f.write('{}\t{}\t{}\n'.format(interval.chrom, interval.start, interval.end))
+				f.write('{}\t{}\t{}\n'.format(chr, start, end))
 
 			tmp_bed = BedTool(tmp_bed_name)
-			overlapping_exon_count[(chr, start, end)] = tmp_bed.intersect(exon_ref).count()
+			self.all_ref_interval_data[(chr, start, end)].exons_spanned = str(tmp_bed.intersect(exon_ref).count())
 
 		os.remove(tmp_bed_name)
 		os.remove(tmp_all_sv_bed_name)
 
-		return overlapping_exon_count
+	def decipher_pop_cnv_freq(decipher_population):
+		
+		cnv_freq = {}
+
+		tmp_all_sv_bed_name = "tmp_all_sv.bed"
+		decipher_pop_bed_name = "decipher_pop_cnv.bed"
+
+		with open(decipher_population) as f:
+			for sv in f:
+				cnv_id, chr, start, end, del_obs, del_freq, del_se, dup_obs, dup_freq, dup_sta, obs, freq, se, cnv_type, samp_size, study = sv.split("\t")
+				cnv_freq[(chr, start, end)] = [cnv_id, del_obs, del_freq, del_se, dup_obs, dup_freq, dup_sta, obs, freq, se, cnv_type, samp_size, study]
+
+		with open(decipher_pop_bed_name, "w") as out_f:
+			for interval in cnv_freq.iterkeys():
+				out_f.write("\t".join(interval))
+
+		all_sv_bed = self.BedTool_make_all_sv(tmp_all_sv_bed_name)
+		pop_bed = BedTool(decipher_pop_bed_name)
 
 	def write_results(self, outfile_name, exon_bed):
 		'''
 			A lot of string manipulation to generate the final CSV file line by line
 		'''
-		overlapping_exon_count = self.make_spanning_exon_dict(exon_bed)
+		self.calc_exons_spanned(exon_bed)
 
 		with open(outfile_name, "w") as out:
 
 			out.write(self.make_header())
 
-			for key in sorted(self.all_sv.iterkeys()):
+			for key in sorted(self.grouped_sv.iterkeys()):
 
 				chr, start, end = key
+				ref_interval = self.all_ref_interval_data[key]
 
-				index, isthere = self.make_sample_list_index(self.all_sv[key])
-				nsamples = str(len(self.all_sv[key]))
-				svtype = self.get_longest_svtype(self.all_sv[key])
-				samp_details = self.make_sample_details(self.all_sv[key])
-				decipher_link = '=hyperlink("https://decipher.sanger.ac.uk/browser#q/{}:{}-{}")'.format(chr, start, end)
-				n_exon_spanned = str(overlapping_exon_count[key])
-				svlen, svmax, svsum, svtop5, svtop10, svmean, dvg, gene = self.all_sv_column_data[key]
+				#sample dependent fields
+				index, isthere = self.make_sample_list_index(self.grouped_sv[key])
+				nsamples = str(len(self.grouped_sv[key]))
+				svtype = self.get_longest_svtype(self.grouped_sv[key])
+				samp_details = self.make_sample_details(self.grouped_sv[key])
 
-				out_line = '{},{},{}\n'.format(",".join([str(chr), str(start), str(end), nsamples, index, gene, svtype, svlen, svmax, svsum, svtop5, svtop10, svmean, dvg, n_exon_spanned, decipher_link]), ",".join(isthere), ",".join(samp_details))
+				out_line = '{},{},{}\n'.format(",".join([str(chr), str(start), str(end), nsamples, index, ref_interval.gene, svtype, ref_interval.svlen, ref_interval.svmax, ref_interval.svsum, ref_interval.svtop5, ref_interval.svtop10, ref_interval.svmean, ref_interval.dgv, ref_interval.exons_spanned, ref_interval.make_decipher_link()]), ",".join(isthere), ",".join(samp_details))
 				out.write(out_line)
 
-def determine_intersect(first_sample_bed, other_sample_bed, other_sample_sv_info, all_sv_records):
+def determine_intersect(a_bed_name, b_bed_name, column_data, all_sv_records):
 	
 	'''
 		Populate all_sv dict with overlapping SVs from another bed file.
@@ -187,9 +230,9 @@ def determine_intersect(first_sample_bed, other_sample_bed, other_sample_sv_info
 		Returns intervals which did not meet overlapping criteria.
 	'''
 
-	a_bed = BedTool(first_sample_bed)
-	b_bed = BedTool(other_sample_bed)
-	grouped_intervals = []
+	a_bed = BedTool(a_bed_name)
+	b_bed = BedTool(b_bed_name)
+	already_grouped_intervals = []
 
 	# find all SVs in b_bed which overlap a SV in a_bed by >=50% and are overlapped by >=50% by the same SV in first_sample_bed
 	# bedtools doc for more param info: https://bedtools.readthedocs.io/en/latest/content/tools/intersect.html?highlight=intersect
@@ -202,42 +245,12 @@ def determine_intersect(first_sample_bed, other_sample_bed, other_sample_sv_info
 		ref_interval = (chr, start, end)
 		new_interval = (samp_chr, samp_start, samp_end)
 
-		if new_interval not in grouped_intervals:
-			all_sv_records.add_interval(ref_interval, new_interval, other_sample_sv_info[new_interval][0], other_sample_sv_info[new_interval][1], samp_name)
-			grouped_intervals.append(new_interval)
+		if new_interval not in already_grouped_intervals:
+			all_sv_records.add_interval(ref_interval, new_interval, column_data, samp_name)
+			already_grouped_intervals.append(new_interval)
 
 	# report all SV in B which did not meet intersection criteria above
 	return b_bed.intersect(a_bed, F=0.5, f=0.5, v=True)
-
-def make_svtype_svlen_dict(scsv):
-
-	'''
-		Store each interval's SVTYPE from CSV file.
-		This info is later used in get_longest_svtype().
-
-		vdict format: vdict[(chr, start, end)] = (svlen, svtype)
-	'''
-
-	vdict = {}
-
-	with open(scsv) as f:
-
-		try: # skip the header
-			next(f)
-		except ValueError: # file is empty
-			return vdict
-
-		for line in csv.reader(f, delimiter=",", quotechar="\""):
-
-			if not line:
-				continue
-
-			chr, start, gt, svtype, svlen, end, sources, nsvt, genes, ann, svmax, svsum, svtop5, svtop10, svmean, dvg = line
-
-			key = (chr, start, end)
-			vdict[key] = (svlen, svtype)
-
-	return vdict
 
 def csv2bed(input_files):
 	'''
@@ -263,10 +276,48 @@ def make_bed_file(interval, bed_dir):
 		for i in interval:
 			f.write(str(i))
 
+def parse_csv(sample_csv, column_data):
+	'''
+		Parse a sample's CSV and store column data associated with an interval to all_ref_interval_data.
+	'''
+	with open(sample_csv) as f:
+
+		try: # skip the header
+			next(f)
+		except ValueError: #file is empty
+			return
+
+		for line in csv.reader(f, delimiter=",", quotechar="\""):
+
+			if not line:
+				continue
+
+			chr, start, gt, svtype, svlen, end, sources, nsvt, gene, ann, svmax, svsum, svtop5, svtop10, svmean, dgv = line			
+			key = (chr, start, end)
+
+			if key not in column_data:
+				newSV = StructuralVariant()
+
+				newSV.chr = chr
+				newSV.start = start
+				newSV.svtype = svtype
+				newSV.svlen = svlen
+				newSV.end = end
+				newSV.gene = gene
+				newSV.svsum = svsum
+				newSV.svmax = svmax
+				newSV.svtop5 = svtop5
+				newSV.svtop10 = svtop10
+				newSV.svmean = svmean
+				newSV.dgv = dgv
+
+				column_data[key] = newSV
+
 def main(exon_bed, outfile_name, input_files, decipher_population, decipher_pathogenic):
 
 	sample_list = csv2bed(input_files)
 	all_sv_records = StructuralVariantRecords(sample_list)
+	all_column_data = {}
 
 	for i, s in enumerate(sample_list):
 
@@ -287,17 +338,15 @@ def main(exon_bed, outfile_name, input_files, decipher_population, decipher_path
 		a_bed = current_dir + sample_list[i] + ".bed"
 		a_csv = sample_list[i] + ".sv.csv"
 
-		all_sv_records.parse_column_data(a_csv)
-
 		for j in range(i, len(sample_list)):
 
 			b_bed = current_dir + sample_list[j] + ".bed"
 			b_csv = sample_list[j] + ".sv.csv"
-			b_svtype_svlen_dict = make_svtype_svlen_dict(b_csv)
+			parse_csv(b_csv, all_column_data)
 
 			# print("pass {}: {} {}".format(str(i), a_bed, b_bed) )
 
-			leftover_sv = determine_intersect(a_bed, b_bed, b_svtype_svlen_dict, all_sv_records)
+			leftover_sv = determine_intersect(a_bed, b_bed, all_column_data, all_sv_records)
 
 			if i != j:
 				new_bed = next_dir + sample_list[j] + ".bed"
@@ -315,7 +364,7 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Generates a structural variant report in CSV format for clincal research')
 	parser.add_argument('-exon_bed', default="/home/naumenko/Desktop/reference_tables/protein_coding_genes.exons.fixed.sorted.bed", help='BED file containing fixed exon positions', required=True)
 	parser.add_argument('-decipher_population', help='TSV file containing population CNVs frequencies from Decipher\'s internal database')
-	parser.add_argument('-decipher_pathogenic', help='TSV file containing deleterious CNVs descriptions from Decipher\'s internal database')
+	parser.add_argument('-decipher_pathogenic', help='TSV file containing deleterious CNVs data from Decipher\'s internal database')
 	parser.add_argument('-o', help='Output file name e.g. -o 180.sv.family.csv', required=True, type=str)
 	parser.add_argument('-i', nargs='+', help='Input file names including .sv.csv extension, e.g. -i 180_230.sv.csv 180_231.sv.csv', required=True)
 	args = parser.parse_args()
