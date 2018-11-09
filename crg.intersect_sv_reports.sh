19a565eae122514df2a711e24b3816b665267412
dennis-kao <d.kao@live.com>
2018-11-09 11:46:33 -0500

    Changed Python version

--- crg.intersect_sv_reports.sh
+++ crg.intersect_sv_reports.sh
@@ -3,12 +3,13 @@
 #PBS -l walltime=00:30:00,nodes=1:ppn=2
 #PBS -joe .
 #PBS -d .
-#PBS -l vmem=10g,mem=10g
+#PBS -l vmem=5g,mem=5g
 
 TODAY=`date +%Y-%m-%d`
 FAMILY_ID=$1
 OUT=${FAMILY_ID}.wgs.sv.${TODAY}.csv
-HGMD=/Users/denniskao/hgmd/hgmd_pro.db
+HGMD=${HOME}/gene_data/hgmd/hgmd_pro.db
+EXON_BED=${HOME}/gene_data/protein_coding_genes.exons.fixed.sorted.bed
 
 if [ ! $2 ]; then
 	IN_FILES=`ls *.sv.csv | tr '\n' ' '`
@@ -19,12 +20,12 @@ fi
 if [[ "$OSTYPE" == *"darwin"* ]]; then
 	PY=python3
 elif [[ "$OSTYPE" == "linux"* ]]; then
-	module load python/3.5.2
+	module load python/3.5.6
 	PY=python
 fi
 
-echo "${PY} /Users/denniskao/crg/crg.intersect_sv_reports.py -exon_bed=/Users/denniskao/protein_coding_genes.exons.fixed.sorted.bed -hgmd=${HGMD} -o=${OUT} -i ${IN_FILES}"
-${PY} /Users/denniskao/crg/crg.intersect_sv_reports.py -exon_bed=/Users/denniskao/protein_coding_genes.exons.fixed.sorted.bed -hgmd=${HGMD} -o=${OUT} -i ${IN_FILES}
+echo "${PY} ${HOME}/crg/crg.intersect_sv_reports.py -exon_bed=${EXON_BED} -hgmd=${HGMD} -o=${OUT} -i ${IN_FILES}"
+${PY} ${HOME}/crg/crg.intersect_sv_reports.py -exon_bed=${EXON_BED} -hgmd=${HGMD} -o=${OUT} -i ${IN_FILES}
 
 if [[ "$OSTYPE" == *"darwin"* ]]; then
 	open -a 'Microsoft Excel' ${OUT}
